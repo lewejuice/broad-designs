76924cb2532fb05130f2fc9e02bf45ae16756dbe
lewejuice <hamiltonlewis@hotmail.com>
2021-02-09 10:02:05 +0000

    order items appear and in order and can be removed

--- order/views.py
+++ order/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from services.models import Services
-from django.urls import reverse
 
 # Create your views here.
 
@@ -18,16 +17,28 @@ def order(request):
 
 
 def add_to_order(request, service_id):
-    """ Add or remove services on users order """
+    """ Add services to users order """
 
     order = request.session.get('order', {})
 
     if service_id in list(order.keys()):
-        # remove_from_order()
-        print("remove from order")
+        remove_from_order(request, service_id, order)
     else:
         order[service_id] = 1
 
     request.session['order'] = order
 
     return redirect(reverse("order"))
+
+
+def remove_from_order(request, service_id, order):
+    """Remove item from the order"""
+
+    try:
+        order.pop(service_id)
+
+        request.session['order'] = order
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
