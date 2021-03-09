94a36efca9678e2acd9bbc2ec441d65070cad4bc
lewejuice <hamiltonlewis@hotmail.com>
2021-03-09 11:59:54 +0000

    added edit and delete service options in management

--- management/urls.py
+++ management/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.management, name='management'),
     path('add-service/', views.add_service, name='add_service'),
+    path('edit-service/<int:service_id>/', views.edit_service, name='edit_service'),
+    path('delete-service/<int:service_id>/', views.delete_service, name='delete_service'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,21 +1,75 @@
-from django.shortcuts import render
-from .forms import ServiceForm
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
+from .forms import ServiceForm
+from services.models import Services
 # Create your views here.
 
 
 def management(request):
     """ A view to navigate to the what we do page """
 
-    return render(request, 'management/management.html')
+    services = Services.objects.all()
+    design_services = Services.objects.filter(category='1')
+    code_services = Services.objects.filter(category='2')
+
+    context = {
+        'services': services,
+        'design_services': design_services,
+        'code_services': code_services,
+    }
+
+    return render(request, 'management/management.html', context)
 
 
 def add_service(request):
-    """ Add a product to the store """
-    form = ServiceForm()
+    """ Add a new service """
+    if request.method == 'POST':
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added service!')
+            return redirect(reverse('add_service'))
+        else:
+            messages.error(request, 'Failed to add service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm()
     template = 'management/add_service.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
+
+def edit_service(request, service_id):
+    """ Edit a service """
+    service = get_object_or_404(Services, pk=service_id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('edit_service'))
+        else:
+            messages.error(request, 'Failed to update service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.name}')
+
+    template = 'management/edit_service.html'
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(request, template, context)
+
+
+def delete_service(request, service_id):
+    """ Delete a product from the store """
+    service = get_object_or_404(Services, pk=service_id)
+    service.delete()
+    messages.success(request, 'Service deleted!')
+
+    return render(request, 'management/management.html')
