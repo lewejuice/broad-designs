05e1fdba03cf713d16af953db6ff348ba22aabc2
lewejuice <hamiltonlewis@hotmail.com>
2021-03-15 13:08:43 +0000

    added styled widget too add/edit portfolio project form

--- portfolio/models.py
+++ portfolio/models.py
@@ -17,7 +17,6 @@ class Portfolio(models.Model):
     name = models.CharField(max_length=254)
     project_date = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
