d7bfc13c8d622d29cbe632b6ab32fee0820f7c37
lewejuice <hamiltonlewis@hotmail.com>
2021-03-17 15:25:25 +0000

    deploying to heroku

--- broaddesigns/settings.py
+++ broaddesigns/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 't3oufw@*j^5=$$l3xxc)%k%1p5q4f57&%*(r@p%63hs0gr0vsc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['broad-designs.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,18 @@ WSGI_APPLICATION = 'broaddesigns.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
