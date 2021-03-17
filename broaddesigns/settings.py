99e7b3505a3434a665e5d97f0a88f32641b57a6f
lewejuice <hamiltonlewis@hotmail.com>
2021-03-17 15:37:06 +0000

    used enviroment variable for secret key

--- broaddesigns/settings.py
+++ broaddesigns/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't3oufw@*j^5=$$l3xxc)%k%1p5q4f57&%*(r@p%63hs0gr0vsc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
