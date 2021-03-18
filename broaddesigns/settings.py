c79aa8a053662d95a9444de9aa99b9931350069d
lewejuice <hamiltonlewis@hotmail.com>
2021-03-18 20:31:28 +0000

    added working email confirmations using outlook

--- broaddesigns/settings.py
+++ broaddesigns/settings.py
@@ -215,4 +215,16 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET_BD', '')
 MAPS_API_KEY = os.getenv('MAPS_API_KEY', '')
-DEFAULT_FROM_EMAIL = 'broadesigns@outlook.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'broadesigns@outlook.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 25
+    EMAIL_HOST = 'smtp-mail.outlook.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
