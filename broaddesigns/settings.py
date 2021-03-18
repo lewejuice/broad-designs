3d1a08d283162a9d48ea3ee77de3a6c3d7505bf1
lewejuice <hamiltonlewis@hotmail.com>
2021-03-18 21:03:08 +0000

    changed email port

--- broaddesigns/settings.py
+++ broaddesigns/settings.py
@@ -223,8 +223,8 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 25
-    EMAIL_HOST = 'smtp-mail.outlook.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.office365.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
