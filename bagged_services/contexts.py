e8107d9cea90e0a6aeac08e7eb8c62ec62542f08
lewejuice <hamiltonlewis@hotmail.com>
2021-03-18 19:18:34 +0000

    fixed issue with adding services to order, with buttons changing colour

--- bagged_services/contexts.py
+++ bagged_services/contexts.py
@@ -12,6 +12,7 @@ def order_contents(request):
     order_total = 0
     order = request.session.get('order', {})
     contact_form = ContactForm()
+    order_objects = []
 
     for service_id in order.items():
         service = get_object_or_404(Services, pk=service_id[0])
@@ -20,10 +21,13 @@ def order_contents(request):
             'service_id': service_id[0],
             'service': service,
         })
+        added_service_id = int(service_id[0])
+        order_objects.append(added_service_id)
 
     context = {
         'order_items': order_items,
         'order_total': order_total,
+        'order_objects': order_objects,
         'contact_form': contact_form,
     }
 
